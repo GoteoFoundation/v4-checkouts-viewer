c5504bd63d0678ccafc66ff153b40d9c46ecf6b4
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-04-22 09:30:21 +0200

    Add component to display money

---
+++ src/lib/components/content/Money.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let amount: number;
+    export let currency: string;
+
+    const formatter = new Intl.NumberFormat("es-ES", {
+        style: "currency",
+        currency,
+    });
+</script>
+
+<span>{formatter.format(amount / 100)}</span>

e1e7e459ddc567b30932f70629df488af5a0f738
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-04-22 08:32:48 +0200

    Add basic content views for users and projects

---
+++ src/lib/components/content/ProjectGlimpse.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let project: any;
+
+    let user = fetch('http://localhost:8090' + project.owner).then(res => res.json());
+</script>
+
+<p>{project.title}</p>
+<p class="text-sm text-muted-foreground">
+    <code>#</code>{project.id}
+</p>
+<p class="text-sm text-muted-foreground">
+    <code>por</code>
+    {#await user then user}
+        @{user.username}
+    {/await}
+</p>
+
---
+++ src/lib/components/content/UserGlimpse.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let user: any;
+</script>
+
+<p>
+    {user.name}
+    <span class="text-muted-foreground">
+        ({user.email})
+    </span>
+</p>
+<p class="text-sm text-muted-foreground">
+    <code>#</code>{user.id}
+</p>
+<p class="text-sm text-muted-foreground">
+    <code>@</code>{user.username}
+</p>

3a346981fc00e2d29f02ca7f6b632b98d548d230
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-04-23 13:10:27 +0200

    Update checkout status

--- src/routes/checkouts/CheckoutsTableStatus.svelte
+++ src/routes/checkouts/CheckoutsTableStatus.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
-    import UserGlimpse from "$lib/components/content/UserGlimpse.svelte";
     import { Badge } from "$lib/components/ui/badge";
 
     export let status: "charged" | "pending";
+
+    let statusValues = {
+        charged: "Cobrado",
+        pending: "Pendiente",
+    };
 
     function isCharged() {
         return status === "charged";
     }
 </script>
 
-<Badge variant={isCharged() ? "default" : "outline"}>{status}</Badge>
+<Badge variant={isCharged() ? "default" : "outline"}>
+    {statusValues[status]}
+</Badge>
